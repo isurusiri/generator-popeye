4701c3e880553e9910ae0e68070e8b6fa514ddcb
isurusiri <isuruanatomy@gmail.com>
2018-08-10 21:50:51 +0530

    code clean up / removed unwanted logs

--- generators/common/syntax-parser.js
+++ generators/common/syntax-parser.js
@@ -5,9 +5,6 @@ var syntaxParser = {
     getRoutesSyntaxTreeByPath: function(path) {
         var code = fs.readFileSync(path, 'utf8');
         var routesSyntax = esprima.parseScript(code);
-        console.log('start of syntax');
-        console.log(JSON.stringify(routesSyntax));
-        console.log('end of syntax');
         return routesSyntax;
     },
 
